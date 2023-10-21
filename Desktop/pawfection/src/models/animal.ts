231284c9c7662540aef29df085af6696cf473c7b
Jason Baek <bkjwjason@gmail.com>
2023-10-22 07:32:57 +0800

    fix animal export

---
+++ Desktop/pawfection/src/models/animal.ts
@@ -0,0 +1,12 @@
+export interface Animal {
+  id: string;
+  name: string;
+  gender: 'Male' | 'Female';
+  dateOfAdmission: Date;
+  dateOfBirth: Date;
+  type: string;
+  breed: string;
+  fed: boolean;
+  played: boolean;
+  medicated: boolean;
+}
