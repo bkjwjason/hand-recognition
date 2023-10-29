c1fec0edf0c79a4896147b32b7347e56e6771069
Jason Baek <bkjwjason@gmail.com>
2023-10-29 16:39:42 +0800

    add search and marking

--- Desktop/pawfection/pages/index.tsx
+++ Desktop/pawfection/pages/index.tsx
@@ -15,7 +15,7 @@ const sampleAnimals: Animal[] = [
       fed: false,
       played: false,
       medicated: false,
-      requiresMedication: false
+      requiresMedication: true
     },
     {
       id: '1111',
@@ -82,19 +82,6 @@ const sampleAnimals: Animal[] = [
         medicated: false,
         requiresMedication: true
       },
-      {
-        id: '8888',
-        name: 'Milo',
-        gender: 'Male',
-        dateOfAdmission: new Date('2022-01-15'),
-        dateOfBirth: new Date('2020-03-20'),
-        type: 'Dog',
-        breed: 'Terrier',
-        fed: false,
-        played: false,
-        medicated: false,
-        requiresMedication: false
-      },
   ];
 
   
@@ -104,15 +91,18 @@ export default function Home() {
   const [selectedAnimal, setSelectedAnimal] = useState<Animal | null>(null); 
   const [command, setCommand] = useState("");
   const [outputMessage, setOutputMessage] = useState<string | null>(null);
+  const [filteredAnimals, setFilteredAnimals] = useState<Animal[]>(sampleAnimals);
 
-  const getMedicationVideoID = (type: string) => {
-    switch (type) {
-        case 'Dog':
-            return 'Y3yHgItqGmo';
-        case 'Cat':
-            return 'Y3meUkY2e4Y';
-        default:
-            return 'sSMQiUnCtZI';
+
+  const getMedicationVideoID = (type: Animal) => {
+    if (type.id === '2222') {
+      return '1TB-M6Le-IU'
+    } else if (type.type === 'Dog') {
+      return 'Y3yHgItqGmo'
+    } else if (type.type === 'Cat') {
+      return 'Y3meUkY2e4Y'
+    } else {
+      return 'sSMQiUnCtZI'
     }
 };
 
@@ -123,7 +113,12 @@ export default function Home() {
 };
 
 const handleCommand = () => {
-  const parts = command.split(" ");
+  let parts = command.split(" /by ");
+  const user = parts[1] || ""; // This is safe because if /by doesn't exist, it'll be undefined.
+  parts = parts[0].split(" ");  
+  const action = parts[0];
+  const index = parseInt(parts[1]);
+  const attributes = parts.slice(2).join(" ").split(",").map(attr => attr.trim());
   if (command.startsWith("add")) {
       const newAnimal: Animal = {
           id: '9999',
@@ -142,23 +137,17 @@ const handleCommand = () => {
       setCommand("");  // Clear the command input
       setOutputMessage("Successfully added!"); 
   } else if (command.startsWith("mark")) {
-      const parts = command.split(" ");
-      if (parts.length >= 3) {
-        const index = parseInt(parts[1]);
-        const attributes = parts.slice(2).join(" ").split(",").map(attr => attr.trim());
-        updateAnimalAttributes(index, attributes, true);
-        setCommand("");  // Clear the command input
-        setOutputMessage("Succesfully marked!")
+        if (index && attributes.length) {
+          updateAnimalAttributes(index, attributes, true, user);
+          setCommand("");  // Clear the command input
+          setOutputMessage("Successfully marked!");
       }
     } else if (command.startsWith("unmark")) {
-      const parts = command.split(" ");
-      if (parts.length >= 3) {
-        const index = parseInt(parts[1]);
-        const attributes = parts.slice(2).join(" ").split(",").map(attr => attr.trim());
-        updateAnimalAttributes(index, attributes, false);
-        setCommand("");  // Clear the command input
-        setOutputMessage("Succesfully unmarked!")
-      }
+          if (index && attributes.length) {
+            updateAnimalAttributes(index, attributes, false, user);
+            setCommand("");  // Clear the command input
+            setOutputMessage("Successfully unmarked!");
+        }
     } else if (command.startsWith("delete")) {
       if(parts.length === 2) {
         const index = parseInt(parts[1]);
@@ -172,30 +161,55 @@ const handleCommand = () => {
       } else {
         setOutputMessage("Invalid delete command format. It should be: delete [index]");
       }
+    } else if (command.startsWith("search")) {
+      const nameToSearch = parts[1];
+      const foundAnimals = animals.filter(animal => animal.name.toLowerCase().includes(nameToSearch.toLowerCase()));
+      
+      if (foundAnimals.length) {
+        setFilteredAnimals(foundAnimals);
+        setSelectedAnimal(foundAnimals[0]);  // Optional: Select the first matched animal.
+        setOutputMessage(`Found ${foundAnimals.length} match(es)!`);
+      } else {
+        setOutputMessage("No animal found with that name.");
+        setFilteredAnimals([]);  // No matches, so clear the list.
+      }
+    } else if (command === "list") {
+      setFilteredAnimals(animals);
+      setOutputMessage("Showing all animals.");
     } else {
       setOutputMessage("Invalid command!");
     }
   };
 
-  const updateAnimalAttributes = (index: number, attributes: string[], value: boolean) => {
+  const updateAnimalAttributes = (index: number, attributes: string[], value: boolean, user:string) => {
     if (index >= 1 && index <= animals.length) {
       let updatedAnimal = {...animals[index - 1]};
+      console.log("Current animal object:", updatedAnimal);
+
       attributes.forEach(attr => {
         switch (attr) {
           case "fed":
             updatedAnimal.fed = value;
+            if (value) updatedAnimal.fedBy = user;
+            else delete updatedAnimal.fedBy;
             break;
           case "played":
             updatedAnimal.played = value;
+            if (value) updatedAnimal.playedBy = user;
+            else delete updatedAnimal.playedBy;
             break;
           case "medicated":
+            console.log("Updating medicated to", value);
             updatedAnimal.medicated = value;
+            if (value) updatedAnimal.medicatedBy = user;
+            else delete updatedAnimal.medicatedBy;
             break;
           default:
             // Handle unknown attributes or do nothing
             break;
         }
       });
+      console.log("Updated animal object:", updatedAnimal);
       const updatedAnimals = [...animals];
       updatedAnimals[index - 1] = updatedAnimal;
       setAnimals(updatedAnimals);
@@ -221,7 +235,7 @@ const handleCommand = () => {
       {outputMessage && <Text color="black" fontWeight="bold">{outputMessage}</Text>}
       <Flex flex="1">
         <VStack flex="1" spacing={5} alignItems="start" maxH="calc(100vh - 200px)" overflowY="auto">
-          {animals.map((animal, index) => (
+          {filteredAnimals.map((animal, index) => (
             <Box key={animal.id} 
                 p={3} 
                 my={2} 
@@ -255,18 +269,26 @@ const handleCommand = () => {
               <Text>Date of Birth: {selectedAnimal.dateOfBirth.toLocaleDateString()}</Text>
               <Text>Type: {selectedAnimal.type}</Text>
               <Text>Breed: {selectedAnimal.breed}</Text>
-              <Text color={selectedAnimal.fed ? "green" : "red"}>Fed: {selectedAnimal.fed ? "Yes" : "No"}</Text>
-              <Text color={selectedAnimal.played ? "green" : "red"}>Played: {selectedAnimal.played ? "Yes" : "No"}</Text>
+              {selectedAnimal?.id === '2222' && (
+                <Text>Injury: <br />1. Pimple looking ulcer at bottom lip. Monitor for growth every session. <br /> 
+                2. Right hand paw red. Rinse at bath area with medicated shampoo, wait 5 minutes and towel dry.</Text>
+              )}
+              {selectedAnimal?.id === '1111' && (
+                <Text>Injury: <br />1. Neck rashes. Check if it is red or raw on the skin, feed liquid medication as instructed on bottle.<br /></Text>
+              )}
+
+              <Text color={selectedAnimal.fed ? "green" : "red"}>Fed: {selectedAnimal.fed ? `Yes ${selectedAnimal.fedBy ? '(by ' + selectedAnimal.fedBy + ')' : ''}` : "No"}</Text>
+              <Text color={selectedAnimal.played ? "green" : "red"}>Played: {selectedAnimal.played ? `Yes ${selectedAnimal.playedBy ? '(by ' + selectedAnimal.playedBy + ')' : ''}` : "No"}</Text>
               {selectedAnimal.requiresMedication && (
                 <>
                       <Text as="span" color={selectedAnimal.medicated ? "green.500" : "red.500"}>
-                          Medicated: {selectedAnimal.medicated ? "Yes" : "No"}
+                          Medicated: {selectedAnimal.medicated ? `Yes ${selectedAnimal.medicatedBy ? '(by ' + selectedAnimal.medicatedBy + ')' : ''}` : "No"}
                       </Text>
                     <Box my={3}>
                         <iframe
                             width="560"
                             height="315"
-                            src={`https://www.youtube.com/embed/${getMedicationVideoID(selectedAnimal.type)}`}
+                            src={`https://www.youtube.com/embed/${getMedicationVideoID(selectedAnimal)}`}
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                         ></iframe>
